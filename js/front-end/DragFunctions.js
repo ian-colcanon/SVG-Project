1802c995f8e800e017a69a7e9abb5d1c50e5f7bf
Ian McCormack <ian.colcanon@gmail.com>
2018-03-14 10:21:54 -0500

    Added the ability to dynamically resize the text window.

---
+++ js/front-end/DragFunctions.js
@@ -0,0 +1,26 @@
+/*global $ document window*/
+$(document).ready(function () {
+    var dragging = false;
+    var container = $("#textContainer");
+    var view = $("#viewContainer");
+    $("#dragbar").mousedown(function (e) {
+        e.preventDefault();
+        dragging = true;
+         
+        $(document).mousemove(function (e) {
+            
+            var percentage = (e.pageX / window.innerWidth) * 100;
+            container.css("width", percentage + "%");    
+            view.css("width", (100-percentage)+"%");
+        });
+    });
+    
+    $(document).mouseup(function () {
+        if(dragging){
+            $(document).unbind('mousemove');
+            dragging = false;
+        }
+        
+    });
+    
+});
