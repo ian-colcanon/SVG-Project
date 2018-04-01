c7bbeab2eab607ed0fac9f97768992b8beb4bb93
Ian McCormack <ian.colcanon@gmail.com>
2018-04-01 11:03:26 -0500

    Consolidated all shape functions into a single paint function.

--- js/front-end/ViewFunctions.js
+++ js/front-end/ViewFunctions.js
@@ -11,39 +11,19 @@ var Engine = {
     xMax: 300,
     yMax: 300,
 
-
-    paint: function (statement) {
-
-        switch (statement.subtype) {
-            case 'RECT':
-                this.rectangle(statement);
-                break;
-            case 'CIRCLE':
-                this.circle(statement);
-                break;
-            case 'ELLIPSE':
-                this.ellipse(statement);
-                break;
-            case 'TEXT':
-                this.text(statement);
-                break;
-            case 'LINE':
-                this.line(statement);
-                break;
-            case 'POLY':
-                this.polyline(statement);
-                break;
-        }
+    paint: function (type, value, attrs) {
+        var element = this.makeSVG(type, value, attrs);
+        this.add(element);
     },
 
-    resize: function (statement) {
+    resize: function (width, height) {
         var display = document.getElementById("view");
         var doc = document.getElementById("draw");
 
-        if (statement != undefined) {
+        if (arguments.length == 2) {
 
-            this.xMax = statement.width.eval();
-            this.yMax = statement.height.eval();
+            this.xMax = width.eval();
+            this.yMax = height.eval();
 
             doc.style.width = this.xMax;
             doc.style.height = this.yMax;
@@ -73,38 +53,7 @@ var Engine = {
     add: function (element) {
         document.getElementById("draw").appendChild(element);
     },
-
-    rectangle: function (rect) {
-        var rectElement = this.makeSVG("rect", null, rect.eval());
-        this.add(rectElement);
-    },
-
-    circle: function (circle) {
-        var circleElement = this.makeSVG("circle", null, circle.eval());
-        this.add(circleElement);
-
-    },
-
-    ellipse: function (ellipse) {
-        var ellipseElement = this.makeSVG("ellipse", null, ellipse.eval());
-        this.add(ellipseElement);
-    },
-
-    text: function (text) {
-        var textElement = this.makeSVG("text", text.getString(), text.eval());
-        this.add(textElement);
-    },
-
-    line: function (line) {
-        var lineElement = this.makeSVG("line", null, line.eval());
-        this.add(lineElement);
-    },
-
-    polyline: function (poly) {
-        var polyElement = this.makeSVG("polyline", null, poly.eval());
-        this.add(polyElement);
-    },
-
+    
     makeSVG: function (tag, value, attributes) {
         var element = document.createElementNS("http://www.w3.org/2000/svg", tag);
 
